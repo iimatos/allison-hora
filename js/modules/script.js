de99326689c4a37f9001a5f0268b182864ca8116
Igor <matosigor00@gmail.com>
2023-01-17 12:38:49 -0300

    #

--- js/modules/script.js
+++ js/modules/script.js
@@ -1,11 +1,11 @@
-import lazyLoad from './lazyload';
+// import lazyLoad from './lazyload';
 import menu from './menu';
 import verticalHeight from './verticalHeight';
 import slide from './slide';
 import handlePositionImage from './positionImage';
 import navigationTabs from './navigationTabs';
 
-lazyLoad();
+// lazyLoad();
 menu();
 verticalHeight();
 slide();
